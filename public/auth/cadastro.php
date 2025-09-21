37bad5f8dbe9a4cf45db9f5d9161ff0b701f005b
Vinícius de Paula <manoseilacara123@gmail.com>
2025-09-21 15:22:33 -0300

    fix: fixed login and cadastro links

--- public/auth/cadastro.php
+++ public/auth/cadastro.php
@@ -26,8 +26,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cadastro do Cliente</title>
-    <link rel="stylesheet" href="../assets/css/cadastro.css" />
-    <link rel="shortcut icon" href="../assets/imgs/logo-globleng.png" type="image/x-icon" />
+    <link rel="stylesheet" href="../../assets/css/cadastro.css" />
+    <link rel="shortcut icon" href="../../assets/imgs/logo-globleng.png" type="image/x-icon" />
 </head>
 
 <body>
--- public/auth/login.php
+++ public/auth/login.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../assets/css/cadastro.css">
+    <link rel="stylesheet" href="../../assets/css/cadastro.css">
     <link
         rel="shortcut icon"
         href="../assets/imgs/logo-globleng.png"
